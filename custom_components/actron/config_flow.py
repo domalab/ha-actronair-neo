8baea45fef9ef0c2f31a45bd26e0df89fd40a3dd
MrD3y5eL <ruaandeysel@outlook.com>
2024-07-09 14:37:19 +1000

    updates

--- custom_components/actron/config_flow.py
+++ custom_components/actron/config_flow.py
@@ -1,49 +1,40 @@
 """Config flow for Actron Neo integration."""
 
 from __future__ import annotations
-from typing import Final, Dict, Any, Optional
+from typing import Any, Dict, Optional
 import logging
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import callback
-import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN
 
-_LOGGER: Final = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
 class ActronNeoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Actron Neo integration."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
-    data: Optional[Dict[str, Any]]
-    
-    def __init__(self):
-        """Initialize."""
-        _LOGGER.debug("%s - ConfigFlowHandler: __init__", DOMAIN)
 
     async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
-        """Invoked when a user initiates a flow via the user interface."""
-        _LOGGER.debug("%s - ConfigFlowHandler: async_step_user: %s", DOMAIN, user_input)
+        """Handle the initial step."""
         errors = {}
 
         if user_input is not None:
             try:
                 # Validate the input
                 await self._test_credentials(user_input["username"], user_input["password"])
-                return self.async_create_entry(
-                    title="Actron Neo", data=user_input
-                )
+                return self.async_create_entry(title="Actron Neo", data=user_input)
             except Exception as e:
                 errors["base"] = "auth"
 
         data_schema = vol.Schema(
             {
-                vol.Required("username", description="Enter your Actron Neo username"): str,
-                vol.Required("password", description="Enter your Actron Neo password"): str,
-                vol.Optional("zones", description="Comma-separated list of zones", default=""): str,
+                vol.Required("username"): str,
+                vol.Required("password"): str,
+                vol.Optional("zones", default=""): str,
             }
         )
         return self.async_show_form(
@@ -77,7 +68,7 @@ class ActronNeoOptionsFlowHandler(config_entries.OptionsFlow):
 
         options = vol.Schema(
             {
-                vol.Optional("zones", default=self.config_entry.options.get("zones", ""), description="Comma-separated list of zones"): str,
+                vol.Optional("zones", default=self.config_entry.options.get("zones", "")): str,
             }
         )
         return self.async_show_form(step_id="init", data_schema=options)
