d5df36132e226687b3cf18df9e5da3edb0e002a0
MrD3y5eL <ruaandeysel@outlook.com>
2024-07-23 07:59:25 +1000

    updates const

--- custom_components/actron_air_neo/const.py
+++ custom_components/actron_air_neo/const.py
@@ -3,3 +3,6 @@ CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_REFRESH_INTERVAL = "refresh_interval"
 CONF_SERIAL_NUMBER = "serial_number"
+DEFAULT_UPDATE_INTERVAL = 60
+API_URL = "https://nimbus.actronair.com.au"
+CMD_SET_SETTINGS = "set-settings"
